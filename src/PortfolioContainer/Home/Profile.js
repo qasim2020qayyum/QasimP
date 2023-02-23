d176085454a4c38f0d7ae6e017336ed77887a927
qasim2020qayyum <qasim20qayyum@gmail.com>
2023-02-23 15:15:37 +0500

    first commit

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import Typical from 'react-typical'
+
+
+const Profile = () => {
+    return (
+        <>
+            <div className='profile-container'>
+                <div className='profile-parent'>
+                    <div className='profile-details'>
+                        <div className='colz'>
+                            <div className='colz-icon'>
+                            <a href='#'>
+                                <i className='bi bi-facebook'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-google-plus'></i>
+
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-instagram'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-youtube'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                            </div>
+
+                        </div>
+                        <div className='profile-details-name'>
+                            <span className='primary-text'>
+                                Hello I am <span className='highlighted-text'>Qasim</span>
+                            </span>
+
+                        </div>
+                        <div className='profile-details-role'>
+                            <span className='primary-text'>
+                                <h1>
+                                    <Typical
+                                        loop={Infinity}
+                                        steps={[
+                                            "Mern stack Developer",
+                                            3000,
+                                            "Full stack Web Developer",
+                                            3000,
+                                            "AWS certified Solution Architect",
+                                            3000,
+                                            "Software Engineer",
+                                            3000,
+                                        ]}
+                                    />
+
+                                </h1>
+                                <span className='profile-role-tagline'>Can build applications with frontend and backend operations.</span>
+
+                            </span>
+
+                        </div>
+                        <div className='profile-options'>
+                            <button className='btn primary-btn'>
+                                Hire Me
+                            </button>
+                            <a href='qasimResume.pdf' download="Qasim Qayyum Resume.pdf">
+                                <button className='btn highlighted-btn'>Get Resume</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='profile-picture'>
+                        <div className='profile-picture-background'>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile
